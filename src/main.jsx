80d5dd94525f9d693e065d112ebeaa6960a70c57
tomyao <tomm.katoh@gmail.com>
2025-12-25 19:38:35 +0900

    Fix Tailwind CSS configuration

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
